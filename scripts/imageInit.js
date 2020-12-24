e59d3306e61942c65c172e97c28fa028f4f7119d
Matt Meagher <mattmeags8@Matts-MacBook-Pro.local>
2020-12-24 13:21:10 -0600

    Add image helper for speed

---
+++ scripts/imageInit.js
@@ -0,0 +1,9 @@
+(function() {
+    var bgImageD, bgImageM;
+
+    bgImageD = new Image();
+    bgImageM = new Image();
+
+    bgImageD.src = './images/main.jpg';
+    bgImageM.src = './images/main-m.jpg';
+})();
